ff646aedd32a402777782f993d154c9ccd1a9c82
tmtuan <tmtuan.projects@gmail.com>
2015-11-27 07:07:33 +0700

    Adds CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:edit, :update, :destroy]
 
   def index
+    @tasks = Task.all
 
   end
 
@@ -9,7 +11,10 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(task_params)
 
+    @task.save
+    redirect_to tasks_path
   end
 
   def edit
@@ -17,10 +22,24 @@ class TasksController < ApplicationController
   end
 
   def update
-
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "Task was successfully updated"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
 
+  private
+  def task_params
+    @task = params.require(:task).permit(:content, :urgent, :important)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
